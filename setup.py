dd408fc33d066fe24df3bbadd93f6b316d0eb412
gcmartinelli <gcmartinelli@gmail.com>
2019-08-31 19:13:46 +0000

    added dependencies

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python3
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-	name='assetfynder',
-	version='0.1',
-	py_modules=['main'],
-	install_requires=[
+	name = 'assetfynder',
+	version = '0.1',
+	packages = find_packages(),
+	include_package_data = True,
+	install_requires = [
 		'Click',
+		'requests',
 		],
-	entry_points='''
+	entry_points = '''
 		[console_scripts]
 		assetfynder=main:cli
 		''',
