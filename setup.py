60a5428062061d16f0112758a5165abdc47482d7
gcmartinelli <none>
2019-12-12 00:28:55 +0000

    fixed setuptools entrypoint (broke with Py3.8 I think)

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-	name = 'assetfynder',
-	version = '0.1',
-	packages = find_packages(),
-	include_package_data = True,
-	install_requires = [
-		'Click',
-		'requests',
-		],
-	entry_points = '''
-		[console_scripts]
-		assetfynder=main:cli
-		''',
-	)
+    name = 'assetfynder',
+    version = '0.1',
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = [
+        'Click',
+        'requests',
+        ],
+        py_modules = [
+                'main',
+                'threatcrowd',
+                'hackertarget',
+                'certspotter',
+                'fetcher',
+                'wayback',
+                ],
+    entry_points = {
+            'console_scripts': [
+                'assetfynder = main:cli'
+                        ]
+                },
+    )
